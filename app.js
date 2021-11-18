81667a265d6884e1a87ad2b2e4cc9ed902d54ecc
Carmen Lucero <lucero.developer@gmail.com>
2021-11-17 19:36:09 -0600

    modified Procfile

--- app.js
+++ app.js
@@ -74,6 +74,6 @@ app.post("/", function(req, res) {
 })  //app.post("/", function(req, res) ENDS
 
 // =============================================================
-app.listen(3000 || process.env.PORT, function() {
+app.listen(process.env.PORT || 3000, function() {
     console.log("Listening on port 3000");
 })
